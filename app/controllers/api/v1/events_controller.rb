01ff31a1a5e4fb481aadf17130fe6c76dd708eb6
Emily Gann <aspentree79@gmail.com>
2020-11-17 13:35:09 -0500

    cleaning up controller and serializer files

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,9 +3,6 @@ class Api::V1::EventsController < ApplicationController
     def index
         events = Event.all
         render json: EventSerializer.new(events)
-    
-        # instead of rendering erb files, we are rendering json file
-        # also, don't need instance variables because we are rendering json
     end
 
     def create
--- app/controllers/api/v1/timelines_controller.rb
+++ app/controllers/api/v1/timelines_controller.rb
@@ -2,21 +2,11 @@ class Api::V1::TimelinesController < ApplicationController
 
     def index
         timelines = Timeline.all
-        # options = {
-        #     # include associated event; must match the wording and associations used in association in the model file for serializer to make those associations
-        #     include: [:event]
-        # }
-        # render json: timelines
         render json: TimelineSerializer.new(timelines)
-        # render json: TimelineSerializer.new(timelines, options)
-
-        # instead of rendering erb files, we are rendering json file
-        # also, don't need instance variables because we are rendering json
     end
 
     def create
         timeline = Timeline.new(timeline_params)
-        byebug
         if timeline.save
             render json: TimelineSerializer.new(timeline), status: :accepted
         else
